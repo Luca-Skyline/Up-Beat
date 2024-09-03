7677337ad1c0fa853a5c358f66452713fd3273cc
ZackWellborn <111790777+ZackWellborn@users.noreply.github.com>
2024-09-03 13:25:47 -0600

    Update passwordGame.java
    updated formatting;

--- src/passwordGame.java
+++ src/passwordGame.java
@@ -8,13 +8,13 @@ public class PasswordGame {
     static String[] my_rules_text = {"Password must be at least five characters.",
         "Password must include a number.",
         "Password must contain a special character.",
+        "Must contain the Roman numeral for the number 500.",
         "Must contain the only word with all 26 letters.",
         "What is light during the day and heavy at night?",
         "What starts and ends with 'E' but only has one letter in it?",
         "What do you do? r1bqkb1r/ppp2ppp/2n2n2/3pp1N1/2B1P3/8/PPPP1PPP/RNBQK2R w KQkq - 0 1",
         "Now for atomic chess too.",
-        "Must contain the name of Max Design Pro's little brother.",
-        "Must contain the Roman numeral for the number 500."
+        "Must contain the name of Max Design Pro's little brother."
     };
     
     public static void main(String[] args) {
@@ -70,46 +70,47 @@ public class PasswordGame {
         if (!hasSpecialCharacter) {
             return 3;
         }
-        
-        //Rule 4: Password must contain "alphabet" (funny)
-        if (!password.contains("alphabet") && !password.contains("Alphabet")) {
+          //Rule 4: Must contain the roman numeral for 500: 
+        if (!password.contains("D")) {
             return 4;
         }
-        
-        //Rule 5: What is light during the day but heavy at night (eyelids)
-        if (!password.contains("eyelids") && !password.contains("Eyelids")) {
+        //Rule 5: Password must contain "alphabet" (funny)
+        if (!password.contains("alphabet") && !password.contains("Alphabet")) {
             return 5;
         }
         
-        //Rule 6: What starts and ends with "E" but only has one letter in it? (envelope or eye)
-        if (!password.contains("envelope") && !password.contains("Envelope") && !password.contains("eye") && !password.contains("Eye")) {
+        //Rule 6: What is light during the day but heavy at night (eyelids)
+        if (!password.contains("eyelids") && !password.contains("Eyelids")) {
             return 6;
         }
         
-        //Rule 7: Must include the best chess move in the position r1bqkb1r/ppp2ppp/2n2n2/3pp1N1/2B1P3/8/PPPP1PPP/RNBQK2R w KQkq - 0 1 (exd5)
-        if (!password.contains("exd5")) {
+        //Rule 7: What starts and ends with "E" but only has one letter in it? (envelope or eye)
+        if (!password.contains("envelope") && !password.contains("Envelope")) {
             return 7;
         }
-            
-        //Rule 8: Must include the best atomic chess move in the same position (nxf7)
-        if (!password.contains("nxf7")) {
+        
+        //Rule 8: Must include the best chess move in the position r1bqkb1r/ppp2ppp/2n2n2/3pp1N1/2B1P3/8/PPPP1PPP/RNBQK2R w KQkq - 0 1 (exd5)
+        if (!password.contains("exd5")) {
             return 8;
         }
-        
-        //Rule 9: Must contain the name of Max Design Pro's little brother?
-        if (!password.contains("jimmy") && !password.contains("Jimmy")) {
+            
+        //Rule 9: Must include the best atomic chess move in the same position (nxf7)
+        if (!password.contains("nxf7")) {
             return 9;
         }
-        //Rule 10: Must contain the roman numeral for 500: 
-        if (!password.contains("D")) {
+        
+        //Rule 10: Must contain the name of Max Design Pro's little brother?
+        if (!password.contains("jimmy") && !password.contains("Jimmy")) {
             return 10;
         }
+        
+        
         System.out.println("Congratulations! Your identity won't get stolen!");
         return 0;
     }
-    
+    //cheat password ;) :: alphabeteyelidsenvelopeexd5nxf7@Djimmy
     public static void clearScreen() {  
         System.out.print("\033[H\033[2J");  
-        System.out.flush();  
+        System.out.flush(); 
     }
 }  

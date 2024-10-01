8d1f614a62b159cf6754eb8b46e123dc9c9a3630
Jenna Linh Tran <124752198+eebic@users.noreply.github.com>
2024-10-01 12:56:22 -0600

    Create Song.java

---
+++ src/musicApp/Song.java
@@ -0,0 +1,26 @@
+public class Song {
+    private Percussion percussion;
+    private Chord[] background;
+    private Note[] baseline;
+    private Note[] melody;
+    
+    Song() {
+        
+    }
+    
+    public export() {
+        
+    }
+    
+    public make() {
+        
+    }
+    
+    private generateChord() {
+        
+    }
+    
+    private generateMelody() {
+        
+    }
+}

91b751eacece832dad36a5bd1d951acfbfb23ce5
Jenna Linh Tran <124752198+eebic@users.noreply.github.com>
2024-10-01 12:55:29 -0600

    Create Note.java

---
+++ src/musicApp/Note.java
@@ -0,0 +1,13 @@
+public class Note {
+    private int value;
+    
+    Note(int value) {
+        this.value = value;
+    }
+    
+    public play() {
+        
+    }
+    
+    public setValue(Type setType) {}
+}

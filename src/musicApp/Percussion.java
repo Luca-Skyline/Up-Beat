a8ef86944acfff58db3d63b5ba43ac0ab5a60d0a
Jenna Linh Tran <124752198+eebic@users.noreply.github.com>
2024-10-01 12:55:47 -0600

    Create Percussion.java

---
+++ src/musicApp/Percussion.java
@@ -0,0 +1,16 @@
+public class Percussion {
+    enum Type = {JAZZ, POP, ETCETERA};
+    Type type;
+    
+    Percussion() {
+        
+    }
+    
+    public play() {
+        
+    }
+    
+    public set_value(Type setType) {
+        
+    }
+}
